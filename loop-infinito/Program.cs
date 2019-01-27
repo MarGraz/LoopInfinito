33a64524e48390192ab70371ab1f20789777ccf0
Marco Graziotti <graziottimarco@gmail.com>
2019-01-27 22:41:32 +0100

    Add project files.

---
+++ loop-infinito/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace loop_infinito
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numRighe;
+
+            //loop infinito per far eseguire il programma di continuo finché l'utente non scrive NO
+            while (true)
+            {
+                Console.Write("Inserisci il numero di righe, altrimenti scrivi no per interrompere: ");
+                numRighe = Console.ReadLine();
+
+                if (numRighe == "no")
+                {
+                    Console.WriteLine("Termina esecuzione, premi Invio ");
+                    Console.ReadLine();
+
+                    break;
+                }
+
+                int righe = Int32.Parse(numRighe);
+
+                string asterisco = "* ";
+
+                List<string> listOfAsterisks = new List<string>();
+
+                int x = 0;
+
+                for (int r = 1; r <= righe; r++)
+                {
+                    //ad ogni ciclo di aggiunta riga (r), aggiungi un asterisco
+                    for (int ast = 1; ast <= r; ast++)
+                    {
+                        listOfAsterisks.Add(asterisco);
+                        Console.Write(listOfAsterisks[x].ToString());
+                    }
+
+                    Console.Write("\n");
+                    x++;                      
+                }
+            }
+        }
+    }
+}
